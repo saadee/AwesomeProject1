0943999c7cc1c08516ef21f30fe184928718873a
saadee <saadee.workspace@gmail.com>
2022-12-06 16:58:47 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,154 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+// import getDirections from 'react-native-google-maps-directions';
+import openMap from 'react-native-open-maps';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const handleGetDirections = () => {
+    console.log('Clicked');
+    const data = {
+      source: {
+        latitude: -33.8356372,
+        longitude: 18.6947617,
+      },
+      destination: {
+        latitude: -33.8600024,
+        longitude: 18.697459,
+      },
+      params: [
+        // {
+        //   key: 'travelmode',
+        //   value: 'driving', // may be "walking", "bicycling" or "transit" as well
+        // },
+        // {
+        //   key: 'dir_action',
+        //   value: 'navigate', // this instantly initializes navigation using the given travel mode
+        // },
+      ],
+      waypoints: [
+        {
+          latitude: -33.8600025,
+          longitude: 18.697452,
+        },
+        {
+          latitude: -33.8600026,
+          longitude: 18.697453,
+        },
+        {
+          latitude: -33.8600036,
+          longitude: 18.697493,
+        },
+      ],
+    };
+    openMap({latitude: 37.865101, longitude: -119.53833});
+    // getDirections(data);
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View>
+          <Text
+            style={{
+              borderWidth: 1,
+              borderColor: 'red',
+              height: 30,
+              fontSize: 20,
+            }}
+            onPress={() => console.log('HI')}>
+            What's up?
+          </Text>
+          <Button onPress={handleGetDirections} title="Get Directions" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
